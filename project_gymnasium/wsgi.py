3bb8b62d456d6bad0e7a9954c4faea2e9251f3cb
KittyKuromi <bunnycute3355@gmail.com>
2020-04-04 13:18:17 +0530

    WSGI Initial

--- project_gymnasium/wsgi.py
+++ project_gymnasium/wsgi.py
@@ -1,15 +1,16 @@
-# To use your own django app use code like this:
+"""
+WSGI config for project_gymnasium project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-#
-## assuming your django settings file is at '/home/iamgirishsawant/mysite/mysite/settings.py'
-## and your manage.py is is at '/home/iamgirishsawant/mysite/manage.py'
-path = '/home/iamgirishsawant/project-gymnsium'
-if path not in sys.path:
-   sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project_gymnsium.settings'
+from django.core.wsgi import get_wsgi_application
 
-## then:
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_gymnasium.settings')
+
+application = get_wsgi_application()
