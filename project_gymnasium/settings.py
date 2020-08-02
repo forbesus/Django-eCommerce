2191646e26e0d06ab3b84dee23b01fb0b56ead6f
KittyKuromi <bunnycute3355@gmail.com>
2020-08-03 03:27:41 +0530

    Live done

--- project_gymnasium/settings.py
+++ project_gymnasium/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_ROOT = '/static/media'
 MEDIA_URL = '/media/'
