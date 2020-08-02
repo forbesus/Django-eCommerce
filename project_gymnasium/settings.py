a75ff640bd9a987b3c84afa14cf2c8c1554b684d
KittyKuromi <bunnycute3355@gmail.com>
2020-08-03 02:43:58 +0530

    Debug false for live

--- project_gymnasium/settings.py
+++ project_gymnasium/settings.py
@@ -52,6 +52,7 @@ LOGGING = {
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +133,7 @@ SESSION_COOKIE_NAME = 'session-project-gymnasium'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
